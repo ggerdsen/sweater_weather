e34c86fbdd863c3d974c5117ba1357d9ab03c5aa
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-07 22:16:06 -0700

    Create helper methods to parse out lat / long

--- app/facade/forecast_facade.rb
+++ app/facade/forecast_facade.rb
@@ -1,5 +1,22 @@
+
 class ForecastFacade
-  def initialize(search_params)
-    ForecastService.get_weather(coords)
+  def self.search(search_params)
+    @location = search_params
+    lat_long = CoordinatesService.get_lat_long(search_params)
+    lat = parsed_lat(lat_long)
+    long = parsed_long(lat_long)
+    weather_data(ForecastService.find_weather(lat, long))
+  end
+  
+  def self.parsed_lat(lat_long)
+    lat_long[:results].first[:locations].first[:latLng][:lat]
+  end
+  
+  def self.parsed_long(lat_long)
+    lat_long[:results].first[:locations].first[:latLng][:lng]
+  end
+  
+  def self.weather_data(data)
+    WeatherData.new(data)
   end
 end
