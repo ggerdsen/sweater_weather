89b11f6c4cf4ce7c7f5b5bc1cf014037b76a65ea
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-09 09:24:55 -0700

    Fix trails spelling...

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
-      get '/trails', to: 'trais#index'
+      get '/trails', to: 'trails#index'
     end
   end
 end
