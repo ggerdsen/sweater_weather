853e0bff98c8ba223dbbbc8d543c387fdaf53184
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-10 14:25:03 -0700

    Adjust facade name in forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    weather_data = ForecastFacade.search(search_params[:location])
+    weather_data = SearchFacade.search_forecast(search_params[:location])
     render json: ForecastSerializer.new(weather_data)
   end
   
