ba545c9be815f668d0f79337c14d40299db0ae5f
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-10 14:25:13 -0700

    Create initial backgrounds controller

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::BackgroundsController < ApplicaitonController
+
+  def index
+    backgrounds = SearchFacade.search_backgrounds(backgrounds_params[:location])
+  end
+  
+  private
+  
+  def backgrounds_params
+    params.permit(:location)
+  end
+
+end
