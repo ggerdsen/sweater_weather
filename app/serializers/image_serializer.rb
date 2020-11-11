a6b067928d45a3e89e62cb8edc883831b2188202
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-11 00:11:21 -0700

    Create road trip serializer

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,10 +1,4 @@
 class ImageSerializer
   include FastJsonapi::ObjectSerializer
-
     attributes :image
-  # attribute :image do |object|
-  #   {location: object.location,
-  #    image_url: object.image_url,
-  #    credit: object.credit}
-  # end
 end
