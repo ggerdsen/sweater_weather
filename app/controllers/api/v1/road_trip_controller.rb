50e4e85a6235a7e3555db84691279662487332bd
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-11 00:10:27 -0700

    Fix params names

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
-
+    user = User.find_by(api_key: trip_plan_params[:api_key])
+    start_city = trip_plan_params[:origin]
+    end_city = trip_plan_params[:destination]
+    trip = SearchFacade.get_info(user, start_city, end_city)
+    render json: RoadTripSerializer.new(trip)
   end
 
   private
 
   def trip_plan_params
-    params.permit(:start_city, :end_city, :api_key)
+    params.permit(:origin, :destination, :api_key)
   end
 end
