c39eaff423ac6c2452fffc2c1fa09f8843add8e3
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-11 00:10:06 -0700

    Add impossible route test

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -38,4 +38,39 @@ RSpec.describe 'Road Trip' do
       expect(trip_info[:data][:attributes][:weather_at_eta][:conditions]).to eq("few clouds")
     end
   end
+  
+  it 'Impossible route' do
+    VCR.use_cassette('impossible_road_trip') do
+      new_user = {email: 'traveller@email.com',
+                  password: 'password',
+                  password_confirmation: 'password'}
+              
+      user = User.create!(new_user)
+      
+      trip_plan = {origin: "Evergreen,CO",
+                   destination: "Honolulu,HI",
+                   api_key: user.api_key}
+                   
+      header = { "CONTENT_TYPE" => "application/json" }
+      post '/api/v1/road_trip', headers: header, params: JSON.generate(trip_plan)
+      trip_info = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(trip_info).to be_a(Hash)
+      expect(trip_info[:data]).to be_a(Hash)
+      expect(trip_info[:data][:id]).to eq(nil)
+      expect(trip_info[:data][:type]).to be_a(String)
+      expect(trip_info[:data][:type]).to eq("road_trip")
+      expect(trip_info[:data][:attributes]).to be_a(Hash)
+      expect(trip_info[:data][:attributes][:start_city]).to be_a(String)
+      expect(trip_info[:data][:attributes][:start_city]).to eq("Evergreen,CO")
+      expect(trip_info[:data][:attributes][:end_city]).to be_a(String)
+      expect(trip_info[:data][:attributes][:end_city]).to eq("Honolulu,HI")
+      expect(trip_info[:data][:attributes][:travel_time]).to be_a(String)
+      expect(trip_info[:data][:attributes][:travel_time]).to eq("impossible route")
+      expect(trip_info[:data][:attributes][:weather_at_eta]).to be_a(String)
+      expect(trip_info[:data][:attributes][:weather_at_eta]).to eq("")
+    end
+  end
 end
