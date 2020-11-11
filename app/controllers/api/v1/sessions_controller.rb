35f0c1f3e34adf5272620765953d8371bd70c324
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-11-10 21:04:06 -0700

    Add sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      render json: UserSerializer.new(user)
+    end
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
+  end
+end
